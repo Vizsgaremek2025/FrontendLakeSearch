727f9725e6ee5e786ecf09baa1fed45e1023a21a
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-02 10:42:58 +0100

    login designolása és logikájának megírása

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  model = {
+    email: '',
+    password: ''
+  };
+  message: string = '';
+  isSuccess: boolean = false;
+  isSubmitted: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.isSubmitted = true;
+    this.message = '';
+
+    // Ellenőrizzük, hogy minden mező ki van-e töltve
+    if (!this.model.email || !this.model.password) {
+      this.message = 'Minden mező kitöltése kötelező!';
+      this.isSuccess = false;
+      return;
+    }
+
+    this.authService.login(this.model).subscribe({
+      next: (successful: boolean) => {
+        if (!successful) {
+          this.message = 'Váratlan hiba történt!';
+          this.isSuccess = false;
+        } else {
+          this.message = 'Sikeres bejelentkezés!'
+          this.isSuccess = true;
+
+          // 2 másodperc után navigálás
+          setTimeout(() => {
+            if (this.authService.loggedinUser) {
+              this.router.navigate(['lakes']);
+            } else {
+              this.router.navigate(['home']);
+            }
+          }, 2000);
+        }
+      },
+      error: (err: any) => {
+        console.error('Bejelentkezési hiba:', err);
+        this.message = err.error?.message ?? 'Hibás email vagy jelszó!';
+        this.isSuccess = false;
+      }
+    });
+  }
 }
