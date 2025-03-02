c028bcfe77f609071d6dd3715e6c3382cbf502ae
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-02 10:12:10 +0100

    models és services mappa létrehozása

---
+++ src/app/models/UserModel.ts
@@ -0,0 +1,8 @@
+export interface UserModel {
+  name:string
+  validTo:Date
+  email: string;
+  roles:string[]
+  token:string
+
+}
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AppComponent } from "../app.component";
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
