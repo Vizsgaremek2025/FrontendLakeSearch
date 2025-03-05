02b0ba2aaaf1bc18b834a44ac6f021d5fbf6c559
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-05 15:42:58 +0100

    Hozzáadtam a fishUrl-t a ConfigService-hez

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -7,6 +7,7 @@ export class ConfigService {
 
   apiUrl = 'http://localhost:3000/auth';
   tavakUrl = 'http://localhost:3000';
+  fishUrl = 'http://localhost:3000/typicalFish';
 
   constructor() { }
 }
