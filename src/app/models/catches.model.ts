60aff4807e6bc9eefe5ac36b5f46d29572c8973f
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-10 15:30:29 +0100

    catches model létrehozása

---
+++ src/app/models/catches.model.ts
@@ -0,0 +1,26 @@
+export interface Catch {
+  _id?: string;
+  fish?: {
+    _id: string;
+    name: string;
+    img?: string;
+  };
+  method?: {
+    _id: string;
+    name: string;
+  };
+  lake?: {
+    _id: string;
+    name: string;
+  };
+  user?: {
+    _id: string;
+    name: string;
+  };
+  length: number;
+  weight: number;
+  date: string;
+  bait: string;
+  description: string;
+  img?: string;
+}
