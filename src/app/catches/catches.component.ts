886b0951af01cd25cb9715831853bf2c5adde413
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-10 16:01:27 +0100

    Az adott tóhoz tartozó fogások formázása és betöltése a backendről táblázatban

--- src/app/catches/catches.component.ts
+++ src/app/catches/catches.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { Catch, CatchResponse } from '../models/catches.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CatchService } from '../services/catch.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-catches',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catches.component.html',
   styleUrl: './catches.component.css'
 })
 export class CatchesComponent {
+  lakeId: number | null = null;
+  catches: Catch[] = [];
+  errorMessage: string = '';
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fishService: CatchService
+  ) {}
+
+  ngOnInit(): void {
+    this.lakeId = this.route.snapshot.params['lakeId'];
+    this.fishService.getFishByLakeId(this.lakeId).subscribe(
+      (response: CatchResponse) => {
+        if (response.success && response.data.length > 0) {
+          this.catches = response.data;
+        } else {
+          this.errorMessage = 'Nincsenek elérhető fogások.';
+        }
+      },
+      (error) => {
+        this.errorMessage = 'Hiba történt a fogások lekérésekor.';
+      }
+    );
+  }
+
+
+
+  goBack(): void {
+    this.router.navigate(['/lake-details', this.lakeId]);
+  }
 }
