bcbe57f2096829f4ffc27d10d327af795c0358ef
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-03-17 18:51:39 +0100

    Felhasználói adatok szerkesztésének megvalósítása modal ablak segítségével és adatok mentése API hívással és designolás

--- src/app/user-catches/user-catches.component.ts
+++ src/app/user-catches/user-catches.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { UserModel } from '../models/UserModel';
 
 @Component({
   selector: 'app-user-catches',
@@ -10,6 +12,55 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-catches.component.css'
 })
 export class UserCatchesComponent {
+  user: UserModel | null = null;
+  editMode = false;
 
+  name = '';
+  email = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getLoggedInUser();
+    if (this.user) {
+      this.name = this.user.name;
+      this.email = this.user.email;
+    }
+  }
+
+  enableEdit() {
+    this.editMode = true;
+  }
+
+  cancelEdit() {
+    if (this.user) {
+      this.name = this.user.name;
+      this.email = this.user.email;
+    }
+    this.editMode = false;
+  }
+
+  openEditModal() {
+    this.editMode = true;
+    if (this.user) {
+      this.name = this.user.name;
+      this.email = this.user.email;
+    }
+  }
+
+  onSubmit() {
+    this.authService.updateUser({ name: this.name, email: this.email }).subscribe({
+      next: () => {
+        if (this.user) {
+          this.user.name = this.name;
+          this.user.email = this.email;
+          localStorage.setItem('user', JSON.stringify(this.user));
+          this.authService['loggedInUserSubject'].next(this.user);
+        }
+        this.editMode = false;
+      },
+      error: (err) => {
+      }
+    });
+  }
 }
