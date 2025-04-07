3ee060117703054ad5315eecc01c0b3e6da9320d
szabobenji60@gmail.com <szabobenji60@gmail.com>
2025-04-07 15:56:53 +0200

    footer component létrehozása és designolása

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavComponent } from "./nav/nav.component";
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, FormsModule, NavComponent,LoginComponent,RegisterComponent],
+  imports: [RouterModule, FormsModule, NavComponent,LoginComponent,RegisterComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
